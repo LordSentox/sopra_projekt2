00bedb3d4d33069fefc7164ffa3212ac4885f622
Arne Dußin <arne.dussin@live.de>
2019-09-05 14:20:01 +0200

    Add docs to PlayerType

--- src/de/sopra/javagame/model/player/PlayerType.java
+++ src/de/sopra/javagame/model/player/PlayerType.java
@@ -1,13 +1,41 @@
 package de.sopra.javagame.model.player;
 
+/**
+ * Der Typ des Spielerfigur
+ */
 public enum PlayerType {
-
+    /**
+     * Der Bote
+     * @see Courier
+     */
     COURIER,
+    /**
+     * Der Taucher
+     * @see Diver
+     */
     DIVER,
+    /**
+     * Der Ingenieur
+     * @see Engineer
+     */
     ENGINEER,
+    /**
+     * Der Abenteurer
+     * @see Explorer
+     */
     EXPLORER,
+    /**
+     * Der Navigator
+     * @see Navigator
+     */
     NAVIGATOR,
+    /**
+     * Der Pilot
+     * @see Pilot
+     */
     PILOT,
+    /**
+     * Es liegt keine der Figuren vor
+     */
     NONE;
-
 }
