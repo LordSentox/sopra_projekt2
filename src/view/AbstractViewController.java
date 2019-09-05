1adc318d3fb5f9a1c2833561b8a5359d230aadfc
sopr048 <sopr048@fiws217.cs.tu-dortmund.de>
2019-09-05 11:44:13 +0200

    added AbstractViewController docs

--- src/view/AbstractViewController.java
+++ src/view/AbstractViewController.java
@@ -1,17 +1,38 @@
 package model.view;
+/*
+ * @author Hannah, Lisa
+
+ * 
+ */
 
 public class AbstractViewController {
-
+	
 	private GameWindow gameWindow;
 
 	private ViewState viewState;
 
+	/**
+	 * gibt zurück welches Fenster(Menu, Settings, InGame, MapEditor, GamePreperatios, HighScores) zurzeit angezeigt wird 
+	 * @return Typ des Fensters
+	 */
 	abstract ViewState getType();
 
+	/**
+	 * neu initailisieren aller GUIs
+	 */
 	abstract void reset();
 
+	
+	/**
+	 * zeigen einer stage
+	 * @param stage Fenster(Menu, Settings, InGame, MapEditor, GamePreperatios, HighScores) welches angezeigt werden soll
+	 */
 	abstract void show(Stage stage);
 
+	/**
+	 * Wechsel zur übergebenen Stage
+	 * @param next Fenster(Menu, Settings, InGame, MapEditor, GamePreperatios, HighScores) welches angezeigt werden soll
+	 */
 	void changeState(ViewState next) {
 
 	}
