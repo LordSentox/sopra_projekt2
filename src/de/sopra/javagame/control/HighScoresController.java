41dfea924e47383320878a1259f5642648bdabd5
Arne Dußin <arne.dussin@live.de>
2019-09-05 12:03:21 +0200

    Add docs to HighScoresController

--- src/de/sopra/javagame/control/HighScoresController.java
+++ src/de/sopra/javagame/control/HighScoresController.java
@@ -1,17 +1,41 @@
 package de.sopra.javagame.control;
 
-import de.sopra.javagame.model.view.HighScoresViewAUI;
+import de.sopra.javagame.view.HighScoresViewAUI;
 
+/**
+ * Lädt, löscht und speichert die Bestenliste im {@link de.sopra.javagame.view.HighScoresViewController}. Hat keinen
+ * Einfluss auf das {@link de.sopra.javagame.model.JavaGame}. Jede Karte hat eine eigene Bestenliste. Die Punkte können
+ * nicht miteinander verglichen werden.
+ */
 public class HighScoresController {
-
     private ControllerChan controllerChan;
 
+    /**
+     * Die  AUI mit der die angezeigten Daten im {@link de.sopra.javagame.view.HighScoresViewController} aktualisiert
+     * werden.
+     */
     private HighScoresViewAUI highScoresViewAUI;
 
+    /**
+     * Erstellt einen neün {@link HighScoresController}
+     * @param highScoresViewAUI das AUI mit dem die Daten übertragen werden sollen.
+     */
+    HighScoresController(HighScoresViewAUI highScoresViewAUI) {
+        this.highScoresViewAUI = highScoresViewAUI;
+    }
+
+    /**
+     * Lädt die aktülle Bestenliste aus einer Bestenlistendatei.
+     * @param mapName Der Name der Karte, für den die Datei geladen werden soll.
+     */
     public void loadHighScores(String mapName) {
 
     }
 
+    /**
+     * Löscht die komplette Bestenliste.
+     * @param mapName Der Name der Karte, dessen Datei zurückgesetzt werden soll.
+     */
     public void resetHighScores(String mapName) {
 
     }
