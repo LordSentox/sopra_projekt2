690b9ddb3cb4bc7c9125506dcc653bc0f38245d0
sopr042 <sopr042@fiws217.cs.tu-dortmund.de>
2019-09-06 15:17:34 +0200

    add some InGameControllerTests

--- tests/de/sopra/javagame/control/InGameUserControllerTest.java
+++ tests/de/sopra/javagame/control/InGameUserControllerTest.java
@@ -2,11 +2,14 @@ package de.sopra.javagame.control;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
 import org.junit.Before;
 import org.junit.Test;
+
 
 import de.sopra.javagame.TestDummy;
 import de.sopra.javagame.model.ArtifactCard;
@@ -16,48 +19,59 @@ import de.sopra.javagame.model.JavaGame;
 import de.sopra.javagame.model.Turn;
 import de.sopra.javagame.model.player.Courier;
 import de.sopra.javagame.model.player.Explorer;
+import de.sopra.javagame.model.player.Navigator;
+import de.sopra.javagame.model.player.Player;
+import de.sopra.javagame.model.player.PlayerType;
+import junit.framework.Assert;
 
 public class InGameUserControllerTest {
 
     private ControllerChan controllerChan; 
     private MapController mapController;
+    private InGameUserController inGameCont;
+    private TestDummy.InGameView inGameView;
     private JavaGame javaGame;
     private Turn turn;
-    private ArtifactCard airCard;
-    private ArtifactCard earthCard;
-    private ArtifactCard waterCard;
     private ArtifactCard fireCard;
+    private ArtifactCard waterCard;
+    private ArtifactCard earthCard;
+    private ArtifactCard airCard;
     private ArtifactCard sandCard;
     private ArtifactCard heliCard;
     private Courier courier;
     private Explorer explorer;
-    private CardStack<ArtifactCard> artifactCardStack ;
+    private Navigator navigator;
+    private CardStack<ArtifactCard> artifactCardStack;
+    private List<ArtifactCard> handCardsExpected;
+    private List moveablePlayers;
     
     @Before
     public void setUp() {
         controllerChan = TestDummy.getDummyControllerChan();
         mapController = controllerChan.getMapController();
+        inGameCont = controllerChan.getInGameUserController();
+        inGameView = (TestDummy.InGameView) controllerChan.getInGameViewAUI();
         javaGame = controllerChan.getJavaGame();
         turn = javaGame.getCurrentTurn();
         artifactCardStack = turn.getArtifactCardStack();
         List<ArtifactCard> cardList = artifactCardStack.draw(28, false);
         
         for (ArtifactCard cur : cardList){
-            if(airCard != null && earthCard != null && waterCard != null && fireCard != null && sandCard != null && heliCard != null){
+            if(fireCard != null && waterCard != null && earthCard != null && airCard != null && sandCard != null && heliCard != null){
                 break;
             }
             
-            if(cur.getType() == ArtifactCardType.AIR){
-                airCard = cur;
-            }
-            if(cur.getType() == ArtifactCardType.EARTH){
-                earthCard = cur;
+            if(cur.getType() == ArtifactCardType.FIRE){
+                fireCard = cur;
             }
             if(cur.getType() == ArtifactCardType.WATER){
                 waterCard = cur;
             }
-            if(cur.getType() == ArtifactCardType.FIRE){
-                fireCard = cur;
+            if(cur.getType() == ArtifactCardType.EARTH){
+                earthCard = cur;
+            }
+            if(cur.getType() == ArtifactCardType.AIR){
+                airCard = cur;
             }
             if(cur.getType() == ArtifactCardType.SANDBAGS){
                 sandCard = cur;
@@ -66,20 +80,93 @@ public class InGameUserControllerTest {
                 heliCard = cur;
             }
         }
+        
+        courier = new Courier("courier", new Point(1,1), turn);
+        explorer = new Explorer("explorer", new Point(2,2), turn);
+        navigator = new Navigator("navigator", new Point(1,1), turn);
+        
+        handCardsExpected = new ArrayList<ArtifactCard>();
+        explorer.getHand().add(fireCard);
+        handCardsExpected.add(fireCard);
+        explorer.getHand().add(waterCard);
+        handCardsExpected.add(waterCard);
+        explorer.getHand().add(earthCard);
+        handCardsExpected.add(earthCard);
+        explorer.getHand().add(airCard);
+        handCardsExpected.add(airCard);
+        
+
+        moveablePlayers = new ArrayList<Player>();
+        moveablePlayers.add(navigator);
+        moveablePlayers.add(courier);
+        
     }
     @Test
     public void testPlayHelicopterCard() {
-        fail("Not yet implemented");
+        //teste mit ungültigem Zielfeld(kein maptile)
+        
+        //teste mit ungültigem Zielfeld(versunkenes maptile)
+
+        //teste ohne helicard
+        inGameCont.playHelicopterCard(PlayerType.EXPLORER, 1, new Point(1,1), new Point(1,9), moveablePlayers);
+        Assert.assertTrue("Die Spieler hätten nicht bewegt werden dürfen", 
+                          inGameView.getNotifications().contains("Du hattest keine Helikopter Karte!"));
+        
+        //teste mit gültigen Daten und einem Spieler (Helicard wurde abgewofen)
+        
+        //teste mit gültigen Daten und mehreren Spielern (Helicard wurde abgewofen)
+        
+        //teste mit Start = Ziel
+        
+        //teste mit HeliCard auf HeliPlatz und nicht alle Artefakte gefunden
+        
+        //teste mit HeliCard auf HeliPlatz und alle Artefakte gefunden
     }
 
     @Test
     public void testPlaySandbagCard() {
-        fail("Not yet implemented");
+        //teste mit ungültigem Zielfeld(kein maptile)
+        
+        //teste mit ungültigem Zielfeld(trockenes maptile)
+        
+        //teste mit ungültigem Zielfeld(versunkenes maptile)
+
+        //teste ohne Sandcard 
+        inGameCont.playHelicopterCard(PlayerType.EXPLORER, 1, new Point(1,1), new Point(1,9), moveablePlayers);
+        Assert.assertTrue("Das Feld hätte nicht trockengelegt werden dürfen", 
+                          inGameView.getNotifications().contains("Du hattest keine Sandsack Karte!"));
+        
+       
+        //teste mit gültigen Daten und einem Spieler (Sandcard wurde abgewofen)
+        
+        //teste mit gültigen Daten und mehreren Spielern (Sandcard wurde abgewofen)
+        
     }
 
     @Test
     public void testDiscardCard() {
-        fail("Not yet implemented");
+        
+        //teste für zu leere Hand (Anzahl Karten <= 5)
+        inGameCont.discardCard(PlayerType.EXPLORER, 1);
+        Assert.assertEquals(handCardsExpected, explorer.getHand());
+        Assert.assertTrue("Der Spieler hätte benachrichtigt werden sollen.", 
+                          inGameView.getNotifications().contains("Es darf keine Karte abgeworfen werden!"));
+        
+        //teste für Hand mit 6 Karten
+        explorer.getHand().add(heliCard);
+        handCardsExpected.add(heliCard);
+        explorer.getHand().add(sandCard);
+        
+        inGameCont.discardCard(PlayerType.EXPLORER, 6);
+        Assert.assertEquals(handCardsExpected, explorer.getHand());
+        
+        //teste für komplett leere Hand (= falscher Index)
+        inGameCont.discardCard(PlayerType.NAVIGATOR, 1);
+        Assert.assertEquals(handCardsExpected, navigator.getHand());
+        Assert.assertTrue("Der Spieler hätte benachrichtigt werden sollen.", 
+                          inGameView.getNotifications().contains("Es gab keine Karte zum abwerfen!"));
+        
+        
     }
 
 }
