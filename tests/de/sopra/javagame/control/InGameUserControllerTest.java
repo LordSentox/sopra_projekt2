957c34c00ce661d095f8acce47557f5f8f1214fb
sopr042 <sopr042@fiws217.cs.tu-dortmund.de>
2019-09-06 13:14:24 +0200

    add setUp for InGameUserConrollerTest

--- tests/de/sopra/javagame/control/InGameUserControllerTest.java
+++ tests/de/sopra/javagame/control/InGameUserControllerTest.java
@@ -2,6 +2,7 @@ package de.sopra.javagame.control;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
 import java.util.Stack;
 
 import org.junit.Before;
@@ -9,6 +10,8 @@ import org.junit.Test;
 
 import de.sopra.javagame.TestDummy;
 import de.sopra.javagame.model.ArtifactCard;
+import de.sopra.javagame.model.ArtifactCardType;
+import de.sopra.javagame.model.CardStack;
 import de.sopra.javagame.model.JavaGame;
 import de.sopra.javagame.model.Turn;
 import de.sopra.javagame.model.player.Courier;
@@ -21,14 +24,14 @@ public class InGameUserControllerTest {
     private JavaGame javaGame;
     private Turn turn;
     private ArtifactCard airCard;
-    private ArtifactCard fireCard;
     private ArtifactCard earthCard;
     private ArtifactCard waterCard;
+    private ArtifactCard fireCard;
     private ArtifactCard sandCard;
     private ArtifactCard heliCard;
     private Courier courier;
     private Explorer explorer;
-    private Stack<ArtifactCard> artifactCardStack ;
+    private CardStack<ArtifactCard> artifactCardStack ;
     
     @Before
     public void setUp() {
@@ -36,7 +39,33 @@ public class InGameUserControllerTest {
         mapController = controllerChan.getMapController();
         javaGame = controllerChan.getJavaGame();
         turn = javaGame.getCurrentTurn();
-        //artifactCardStack = turn.getArtifactCardStack();
+        artifactCardStack = turn.getArtifactCardStack();
+        List<ArtifactCard> cardList = artifactCardStack.draw(28, false);
+        
+        for (ArtifactCard cur : cardList){
+            if(airCard != null && earthCard != null && waterCard != null && fireCard != null && sandCard != null && heliCard != null){
+                break;
+            }
+            
+            if(cur.getType() == ArtifactCardType.AIR){
+                airCard = cur;
+            }
+            if(cur.getType() == ArtifactCardType.EARTH){
+                earthCard = cur;
+            }
+            if(cur.getType() == ArtifactCardType.WATER){
+                waterCard = cur;
+            }
+            if(cur.getType() == ArtifactCardType.FIRE){
+                fireCard = cur;
+            }
+            if(cur.getType() == ArtifactCardType.SANDBAGS){
+                sandCard = cur;
+            }
+            if(cur.getType() == ArtifactCardType.HELICOPTER){
+                heliCard = cur;
+            }
+        }
     }
     @Test
     public void testPlayHelicopterCard() {
