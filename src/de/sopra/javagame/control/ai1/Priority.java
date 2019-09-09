c11c4808a3357250a9388e55ab09b6732ac6cfe8
Juyas <juyasetgames@gmail.com>
2019-09-09 15:07:39 +0200

    Add docs to Priority

--- src/de/sopra/javagame/control/ai1/Priority.java
+++ src/de/sopra/javagame/control/ai1/Priority.java
@@ -1,5 +1,7 @@
 package de.sopra.javagame.control.ai1;
 
+import de.sopra.javagame.control.AIController;
+
 /**
  * <h1>projekt2</h1>
  *
@@ -9,6 +11,22 @@ package de.sopra.javagame.control.ai1;
  */
 public interface Priority {
 
-    double getPriority(Action action);
+    /**
+     * Soll einen Wert für die gegebene Aktion berechnen, der die Wichtigkeit der Aktion repräsentiert
+     *
+     * @param action  die zu bewertende Aktion
+     * @param control der AIController mit allen wichtigen Inhalten
+     * @return ein Bewertung der Aktion. Je größer die Zahl, desto wichtiger
+     */
+    double getPriority(Action action, AIController control);
+
+    /**
+     * Ein Gewichtungswertswert, der als Multiplikator einfließt.
+     *
+     * @return der Multiplikator zur Feineinstellung von Gewichtung
+     */
+    default double weight() {
+        return 1;
+    }
 
 }
