400e1aa76428dada036e8a9b5d13c7e6c472b6ef
Juyas <juyasetgames@gmail.com>
2019-09-09 13:59:16 +0200

    Add AIController Add Priority1 to create priority package Add TurnDecision1 to create decision package

---
+++ src/de/sopra/javagame/control/AIController.java
@@ -0,0 +1,14 @@
+package de.sopra.javagame.control;
+
+/**
+ * <h1>projekt2</h1>
+ *
+ * @author Julius Korweck
+ * @version 09.09.2019
+ * @since 09.09.2019
+ */
+public class AIController {
+
+
+
+}
