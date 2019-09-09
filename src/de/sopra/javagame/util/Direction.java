31e8b1cebeac06b1093abb980eab6828295b7c64
Juyas <juyasetgames@gmail.com>
2019-09-09 17:47:11 +0200

    Add translation method to Direction to modify Point much easier

--- src/de/sopra/javagame/util/Direction.java
+++ src/de/sopra/javagame/util/Direction.java
@@ -1,15 +1,37 @@
 package de.sopra.javagame.util;
 
+import java.awt.*;
+
 /**
  * Die Richtungen, in die ein Spieler ohne besondere Bewegungsfähigkeiten bewegt werden kann.
  */
 public enum Direction {
-    /** Oben */
-    UP,
-    /** Links */
-    LEFT,
-    /** Unten */
-    DOWN,
-    /** Rechts */
-    RIGHT;
+    /**
+     * Oben
+     */
+    UP(0, -1),
+    /**
+     * Links
+     */
+    LEFT(-1, 0),
+    /**
+     * Unten
+     */
+    DOWN(0, 1),
+    /**
+     * Rechts
+     */
+    RIGHT(1, 0);
+
+    private int xOffset, yOffset;
+
+    Direction(int xOffset, int yOffset) {
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+    }
+
+    public Point translate(Point point) {
+        return new Point(point.x + xOffset, point.y + yOffset);
+    }
+
 }
