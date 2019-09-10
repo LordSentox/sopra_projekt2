d3731c5048f44b5d232205c528e9123f438215d8
sopr049 <sopr049@fiws220.cs.tu-dortmund.de>
2019-09-10 14:30:08 +0200

    create DiscardRarestTreasureCards

---
+++ src/de/sopra/javagame/control/ai2/decisions/DiscardRarestTreasureCards.java
@@ -0,0 +1,27 @@
+package de.sopra.javagame.control.ai2.decisions;
+
+import java.util.Comparator;
+import java.util.List;
+
+import de.sopra.javagame.control.ai2.Decision;
+import de.sopra.javagame.model.ArtifactCard;
+import de.sopra.javagame.model.ArtifactCardType;
+import de.sopra.javagame.model.player.Player;
+
+/**
+ * <h1>projekt2</h1>
+ *
+ * @author Niklas Falke
+ * @version 10.09.2019
+ * @since 10.09.2019
+ */
+public class DiscardRarestTreasureCards extends Decision {
+    @Override
+    public Decision decide() {
+        return this;
+    }
+    @Override
+    public void act() {
+        //TODO
+    }
+}
