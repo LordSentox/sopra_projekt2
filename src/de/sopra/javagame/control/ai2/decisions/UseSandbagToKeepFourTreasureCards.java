067e59e2904af9e65cbb855a2689ac71f57e7b8e
sopr049 <sopr049@fiws220.cs.tu-dortmund.de>
2019-09-10 11:18:51 +0200

    Implement UseSandbagToKeepFourTreasureCards decision

--- src/de/sopra/javagame/control/ai2/decisions/UseSandbagToKeepFourTreasureCards.java
+++ src/de/sopra/javagame/control/ai2/decisions/UseSandbagToKeepFourTreasureCards.java
@@ -1,7 +1,12 @@
 package de.sopra.javagame.control.ai2.decisions;
 
+import java.util.List;
+
 import de.sopra.javagame.control.AIController;
 import de.sopra.javagame.control.ai2.Decision;
+import de.sopra.javagame.model.ArtifactCard;
+import de.sopra.javagame.model.ArtifactCardType;
+import de.sopra.javagame.model.player.Player;
 
 /**
  * <h1>projekt2</h1>
@@ -10,13 +15,41 @@ import de.sopra.javagame.control.ai2.Decision;
  * @version 10.09.2019
  * @since 10.09.2019
  */
-public class UseSandbagToKeepFourTreasureCards implements Decision {
+public class UseSandbagToKeepFourTreasureCards extends Decision {
     @Override
-    public Decision decide(AIController control) {
+    public Decision decide() {
+        Player activePlayer = control.getActivePlayer();
+        List<ArtifactCard> activeHand = activePlayer.getHand();
+        List<Player> allPlayers = control.getAllPlayers();
+        boolean hasSand=false;
+        for(ArtifactCard sand: activeHand) { 
+            if(sand.getType()==ArtifactCardType.SANDBAGS){hasSand=true;}
+        }    
+        if(!hasSand){
+            return null;
+        }
+        int water=0; 
+        int fire=0;
+        int earth=0;
+        int air=0;
+        for(ArtifactCard card: activeHand) { 
+            if(card.getType()==ArtifactCardType.AIR) {
+                air++;
+            } else if(card.getType()==ArtifactCardType.EARTH) {
+                earth++;
+            } else if(card.getType()==ArtifactCardType.FIRE) {
+                fire++;
+            } else if(card.getType()==ArtifactCardType.WATER) {
+                water++;
+            }
+        }
+        if(air==4||fire==4||earth==4||air==4){
+            return this;
+        }    
         return null;
     }
     @Override
-    public void act(AIController control) {
+    public void act() {
         //TODO
     }
 }
