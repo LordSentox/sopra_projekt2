328da6bdb131794205bda94c34b9af475c7fefda
Juyas <juyasetgames@gmail.com>
2019-09-10 15:54:16 +0200

    Rearrangement and style stuff

--- src/de/sopra/javagame/control/ai2/decisions/MoveOntoTreasureCollectionPointIfPlayerHasFour.java
+++ src/de/sopra/javagame/control/ai2/decisions/MoveOntoTreasureCollectionPointIfPlayerHasFour.java
@@ -1,12 +1,12 @@
 package de.sopra.javagame.control.ai2.decisions;
 
-import java.util.List;
-
 import de.sopra.javagame.control.ai2.Decision;
 import de.sopra.javagame.model.ArtifactCard;
 import de.sopra.javagame.model.ArtifactCardType;
 import de.sopra.javagame.model.ArtifactType;
 import de.sopra.javagame.model.player.Player;
+
+import java.util.List;
 
 /**
  * @author Niklas Falke
@@ -18,33 +18,34 @@ public class MoveOntoTreasureCollectionPointIfPlayerHasFour extends Decision {
     public Decision decide() {
         Player activePlayer = control.getActivePlayer();
         List<ArtifactCard> activeHand = activePlayer.getHand();
-        int water=0; 
-        int fire=0;
-        int earth=0;
-        int air=0;
-        for(ArtifactCard card: activeHand) { 
-            if(card.getType()==ArtifactCardType.AIR) {
+        int water = 0;
+        int fire = 0;
+        int earth = 0;
+        int air = 0;
+        for (ArtifactCard card : activeHand) {
+            if (card.getType() == ArtifactCardType.AIR) {
                 air++;
-            } else if(card.getType()==ArtifactCardType.EARTH) {
+            } else if (card.getType() == ArtifactCardType.EARTH) {
                 earth++;
-            } else if(card.getType()==ArtifactCardType.FIRE) {
+            } else if (card.getType() == ArtifactCardType.FIRE) {
                 fire++;
-            } else if(card.getType()==ArtifactCardType.WATER) {
+            } else if (card.getType() == ArtifactCardType.WATER) {
                 water++;
             }
         }
-       ArtifactType type=control.getTile(activePlayer.getPosition()).getProperties().getHidden();
-        if(air==4 && !(type==ArtifactType.AIR)){
+        ArtifactType type = control.getTile(activePlayer.getPosition()).getProperties().getHidden();
+        if (air == 4 && !(type == ArtifactType.AIR)) {
             return this;
-        }else if(earth==4 && !(type==ArtifactType.EARTH)){
+        } else if (earth == 4 && !(type == ArtifactType.EARTH)) {
             return this;
-        }else if(fire==4 && !(type==ArtifactType.FIRE)){
+        } else if (fire == 4 && !(type == ArtifactType.FIRE)) {
             return this;
-        }else if(water==4 && !(type==ArtifactType.WATER)){
+        } else if (water == 4 && !(type == ArtifactType.WATER)) {
             return this;
         }
         return null;
     }
+
     @Override
     public void act() {
         //TODO
