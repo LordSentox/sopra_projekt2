ec55db799134a9bf23f2fb370a69b8d9c00b0833
sopr049 <sopr049@fiws220.cs.tu-dortmund.de>
2019-09-10 10:12:19 +0200

    Improve some decisions

---
+++ src/de/sopra/javagame/control/ai2/decisions/DiscardTreasureCardOfWhichareTwoInDiscard.java
@@ -0,0 +1,26 @@
+package de.sopra.javagame.control.ai2.decisions;
+
+import de.sopra.javagame.control.AIController;
+import de.sopra.javagame.control.ai2.Decision;
+import de.sopra.javagame.model.ArtifactCard;
+import de.sopra.javagame.model.ArtifactCardType;
+import de.sopra.javagame.model.ArtifactType;
+import de.sopra.javagame.model.Turn;
+import de.sopra.javagame.model.player.Player;
+
+import java.util.EnumSet;
+import java.util.List;
+
+public class DiscardTreasureCardOfWhichareTwoInDiscard implements Decision {
+    @Override
+    public Decision decide(AIController control) {
+         //TODO
+        
+        return null;
+    }
+    
+    @Override
+    public void act(AIController control) {
+        //TODO
+    }
+}
