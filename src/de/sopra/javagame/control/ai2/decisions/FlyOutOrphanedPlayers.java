a74cf05b3c3f775cee7ad04d10566e27e0614cf5
sopr044 <sopr044@fiws219.cs.tu-dortmund.de>
2019-09-10 15:04:36 +0200

    Implement FlyOutOrphanedPlayers

---
+++ src/de/sopra/javagame/control/ai2/decisions/FlyOutOrphanedPlayers.java
@@ -0,0 +1,46 @@
+package de.sopra.javagame.control.ai2.decisions;
+
+import java.util.List;
+
+import de.sopra.javagame.control.ai2.Decision;
+import de.sopra.javagame.model.MapTile;
+import de.sopra.javagame.model.player.Player;
+import de.sopra.javagame.model.player.PlayerType;
+import de.sopra.javagame.util.Pair;
+import de.sopra.javagame.util.Point;
+
+/**
+ * <h1>projekt2</h1>
+ *
+ * @author Melanie Arnds
+ * @version 10.09.2019
+ * @since 10.09.2019
+ */
+
+public class FlyOutOrphanedPlayers extends Decision{
+
+    @Override
+    public Decision decide() {
+        List<Player> allPlayers = control.getAllPlayers();
+        
+        Pair<Point, MapTile> informationLandingSite = control.getTile(PlayerType.PILOT);
+        Point landingSitePosition = informationLandingSite.getLeft();
+        
+        for(Player player: allPlayers){
+            if(player.getPosition().equals(landingSitePosition)){
+                return null;
+            }
+            if(player.legalMoves(true).isEmpty()){
+                return this;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void act() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
