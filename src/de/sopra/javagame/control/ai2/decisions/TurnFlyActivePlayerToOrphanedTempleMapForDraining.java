a2a3fda28606924e387d2c4111869bfb4e9127b9
Juyas <juyasetgames@gmail.com>
2019-09-11 19:09:03 +0200

    Do crazy removal of helpful things like Political Methods Demonstrate

--- src/de/sopra/javagame/control/ai2/decisions/TurnFlyActivePlayerToOrphanedTempleMapForDraining.java
+++ src/de/sopra/javagame/control/ai2/decisions/TurnFlyActivePlayerToOrphanedTempleMapForDraining.java
@@ -27,28 +27,26 @@ public class TurnFlyActivePlayerToOrphanedTempleMapForDraining extends Decision 
         if (!control.anyPlayerHasCard(ArtifactCardType.HELICOPTER)) {
             return null;
         }
-
         if (!hasValidActions(1)) {
             return null;
         }
-
         List<Pair<Point, MapTile>> templeList = control.getTemples();
-
         //filter non-flooded tiles
         templeList = templeList.stream().filter(pair -> pair.getRight().getState() == FLOODED).collect(Collectors.toList());
-
         for (Pair<Point, MapTile> temple : templeList) {
             Point orphanedTemplePoint = temple.getLeft();
-
             List<Point> surroundingPoints = surroundingPoints(orphanedTemplePoint, true);
-
             List<MapTile> surroundingTiles = surroundingPoints.stream().map(control::getTile).collect(Collectors.toList());
-
             //wenn eins nicht GONE ist
             if (!checkAll(tile -> tile.getState() == GONE, surroundingTiles)) {
                 continue;
             }
+            return this;
+        }
+        return null;
+    }
 
+    //alte version
 //            if (!((northernNeighbour == null || northernNeighbour.getState() == MapTileState.GONE)
 //                    && (northEasternNeighbour == null || northEasternNeighbour.getState() == MapTileState.GONE)
 //                    && (easternNeighbour == null || easternNeighbour.getState() == MapTileState.GONE)
@@ -59,11 +57,6 @@ public class TurnFlyActivePlayerToOrphanedTempleMapForDraining extends Decision 
 //                    && (northWesternNeighbour == null || northWesternNeighbour.getState() == MapTileState.GONE))) {
 //                continue;
 //            }
-
-            return this;
-        }
-        return null;
-    }
 
     @Override
     public void act() {
--- src/de/sopra/javagame/control/ai2/decisions/TurnMoveToOrphanedTempleMapTileForDraining.java
+++ src/de/sopra/javagame/control/ai2/decisions/TurnMoveToOrphanedTempleMapTileForDraining.java
@@ -24,40 +24,34 @@ public class TurnMoveToOrphanedTempleMapTileForDraining extends Decision {
 
     @Override
     public Decision decide() {
-
         if (hasValidActions(0, 1)) {
             return null;
         }
-
         PlayerType activePlayerType = player().getType();
-
         //Nur für Diver und Pilot relevant
         if (none(activePlayerType == PlayerType.DIVER, activePlayerType == PlayerType.PILOT)) {
             return null;
         }
-
         List<Pair<Point, MapTile>> templeList = control.getTemples();
-
         //filter non-flooded tiles
         templeList = templeList.stream().filter(pair -> pair.getRight().getState() == FLOODED).collect(Collectors.toList());
 
         for (Pair<Point, MapTile> temple : templeList) {
             Point orphanedTemplePoint = temple.getLeft();
-
             List<Point> inOneMovedrainablePositionslist = control.getDrainablePositionsOneMoveAway(orphanedTemplePoint, activePlayerType);
-
             if (!inOneMovedrainablePositionslist.contains(orphanedTemplePoint)) {
                 return null;
             }
-
             List<Point> surroundingPoints = surroundingPoints(orphanedTemplePoint, true);
-
             List<MapTile> surroundingTiles = surroundingPoints.stream().map(control::getTile).collect(Collectors.toList());
-
             //wenn eins nicht GONE ist
             if (!checkAll(tile -> tile.getState() == GONE, surroundingTiles)) {
                 continue;
             }
+            return this;
+        }
+        return null;
+    }
 
             //alte version
 //            if (!((northernNeighbour == null || northernNeighbour.getState() == MapTileState.GONE)
@@ -70,14 +64,6 @@ public class TurnMoveToOrphanedTempleMapTileForDraining extends Decision {
 //                    && (northWesternNeighbour == null || northWesternNeighbour.getState() == MapTileState.GONE))) {
 //                continue;
 //            }
-
-            return this;
-
-        }
-
-        return null;
-
-    }
 
     @Override
     public void act() {
