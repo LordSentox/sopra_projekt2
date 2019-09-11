80ce03b41f01dbd32ef235986f9ffc2d0c59b7ed
sopr049 <sopr049@fiws220.cs.tu-dortmund.de>
2019-09-11 15:43:45 +0200

    create new Decisions

--- src/de/sopra/javagame/control/ai2/decisions/TurnMoveToDrainTile.java
+++ src/de/sopra/javagame/control/ai2/decisions/TurnMoveToDrainTile.java
@@ -2,6 +2,7 @@ package de.sopra.javagame.control.ai2.decisions;
 
 import de.sopra.javagame.control.ai2.Decision;
 import de.sopra.javagame.model.player.Player;
+
 /**
  * <h1>projekt2</h1>
  *
@@ -9,23 +10,20 @@ import de.sopra.javagame.model.player.Player;
  * @version 11.09.2019
  * @since 11.09.2019
  */
-public class TurnMoveIfMovingCouldDrainTwoTiles extends Decision {
+public class TurnMoveToDrainTile extends Decision {
     @Override
     public Decision decide() {
-        if(!hasValidActions(3)) {
-            return null;
-        }
         Player activePlayer = control.getActivePlayer();
-        if(!(activePlayer.drainablePositions().size()<2)) {
+        if(!(activePlayer.drainablePositions().size()==0)) {
             return null;
         }
-            if(control.getDrainablePositionsOneMoveAway(activePlayer.getPosition(),
-               activePlayer.getType()).size()>1){
-                return this;
-            }
+        if(control.getDrainablePositionsOneMoveAway(activePlayer.getPosition(),
+            activePlayer.getType()).size()>0){
+            return this;
+        }
         return null;
     }
-    
+
     @Override
     public void act() {
         //TODO
