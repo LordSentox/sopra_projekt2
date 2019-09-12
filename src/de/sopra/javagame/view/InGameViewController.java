11d984d7d7042321a82a558e170344d3ed5d0ea8
sopr048 <sopr048@fiws215.cs.tu-dortmund.de>
2019-09-12 11:24:55 +0200

    edit GameWindow

--- src/de/sopra/javagame/view/InGameViewController.java
+++ src/de/sopra/javagame/view/InGameViewController.java
@@ -60,7 +60,6 @@ public class InGameViewController extends AbstractViewController implements InGa
         activePlayerTypeImageView.setImage(TextureLoader.getPlayerTexture(PlayerType.DIVER));
         activePlayerTypeImageView.setPreserveRatio(true);
         activePlayerTypeImageView.setFitWidth(ACTIVE_CARD_SIZE);
-        activePlayerTypeImageView.setFitHeight(ACTIVE_CARD_SIZE);
         activePlayerTypeImageView.setVisible(true);
         
         playerOneTypeImageView.setImage(TextureLoader.getPlayerTexture(PlayerType.PILOT));
@@ -87,14 +86,17 @@ public class InGameViewController extends AbstractViewController implements InGa
         turnSpinnerWithoutMarkerImageView.setFitWidth(SPINNER_SIZE);
         turnSpinnerWithoutMarkerImageView.setFitHeight(SPINNER_SIZE);
         turnSpinnerWithoutMarkerImageView.setVisible(true);
+        //Bild um 72 Grad drehen
         turnSpinnerWithoutMarkerImageView.setRotate(72.0);
-        turnSpinnerWithoutMarkerImageView.getStyleClass().add("Artifact_Fire");
-        //TODO Bild um 72 Grad drehen
+        turnSpinnerWithoutMarkerImageView.getStyleClass().add("CardView");
         markerForSpinnerImageView.setImage(TextureLoader.getSpinnerMarker());
+        
         markerForSpinnerImageView.setPreserveRatio(true);
         markerForSpinnerImageView.setFitWidth(SPINNER_SIZE);
         markerForSpinnerImageView.setFitHeight(SPINNER_SIZE);
         markerForSpinnerImageView.setVisible(true);
+        markerForSpinnerImageView.getStyleClass().add("CardView");
+        
         
         
         
