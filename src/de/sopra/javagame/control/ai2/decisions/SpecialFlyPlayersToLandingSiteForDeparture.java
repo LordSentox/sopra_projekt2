67b5b2dcb63d7224c995111b796df2ea001b6d0a
sopr044 <sopr044@fiws219.cs.tu-dortmund.de>
2019-09-12 15:28:08 +0200

    Implement SpecialFlyPlayersToLandingSiteForDeparture

---
+++ src/de/sopra/javagame/control/ai2/decisions/SpecialFlyPlayersToLandingSiteForDeparture.java
@@ -0,0 +1,34 @@
+package de.sopra.javagame.control.ai2.decisions;
+
+import de.sopra.javagame.model.ArtifactCardType;
+import de.sopra.javagame.model.MapTile;
+import de.sopra.javagame.model.player.Player;
+import de.sopra.javagame.model.player.PlayerType;
+import de.sopra.javagame.util.Pair;
+import de.sopra.javagame.util.Point;
+
+import java.util.List;
+
+/**
+ * <h1>projekt2</h1>
+ *
+ * @author Melanie Arnds
+ * @version 12.09.2019
+ * @since 12.09.2019
+ */
+
+public class SpecialFlyPlayersToLandingSiteForDeparture extends Decision{
+
+    @Override
+    public Decision decide() {
+        // TODO use BFS to decide if to fly out Players or let them walk
+        return null;
+    }
+
+    @Override
+    public void act() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
