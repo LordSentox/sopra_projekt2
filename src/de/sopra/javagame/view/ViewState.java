73e491ab218a153842260fe721c8fb50d399d954
Juyas <juyasetgames@gmail.com>
2019-09-12 15:09:15 +0200

    Make ViewState beautiful again

--- src/de/sopra/javagame/view/ViewState.java
+++ src/de/sopra/javagame/view/ViewState.java
@@ -1,35 +1,27 @@
 package de.sopra.javagame.view;
 
 public enum ViewState {
+
     MENU,
     SETTINGS,
     IN_GAME_SETTINGS,
-    IN_GAME,
-    MAP_EDITOR,
+    IN_GAME(true),
+    MAP_EDITOR(true),
     GAME_PREPARATIONS,
     HIGH_SCORES,
     CLOSE;
 
+    private final boolean fullscreen;
+
+    ViewState(boolean fullscreen) {
+        this.fullscreen = fullscreen;
+    }
+
+    ViewState() {
+        this(false);
+    }
+
     public boolean isFullscreen() {
-        switch (this) {
-        case CLOSE:
-            return false;
-        case GAME_PREPARATIONS:
-            return false;
-        case HIGH_SCORES:
-            return false;
-        case IN_GAME:
-            return true;
-        case MAP_EDITOR:
-            return true;
-        case MENU:
-            return false;
-        case SETTINGS:
-            return false;
-        case IN_GAME_SETTINGS:
-            return false;
-        default: 
-            return false;
-        }
+        return fullscreen;
     }
 }
