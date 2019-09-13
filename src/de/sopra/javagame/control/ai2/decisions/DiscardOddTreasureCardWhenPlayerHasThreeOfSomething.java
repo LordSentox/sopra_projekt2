7f63675f802a30be90cbe353c4dd95cc058365a4
sopr049 <sopr049@fiws220.cs.tu-dortmund.de>
2019-09-13 15:45:51 +0200

    add acts to discards

--- src/de/sopra/javagame/control/ai2/decisions/DiscardOddTreasureCardWhenPlayerHasThreeOfSomething.java
+++ src/de/sopra/javagame/control/ai2/decisions/DiscardOddTreasureCardWhenPlayerHasThreeOfSomething.java
@@ -3,6 +3,7 @@ package de.sopra.javagame.control.ai2.decisions;
 import de.sopra.javagame.control.ai.ActionQueue;
 import de.sopra.javagame.control.ai.EnhancedPlayerHand;
 import de.sopra.javagame.control.ai2.DoAfter;
+import de.sopra.javagame.model.ArtifactCardType;
 
 import static de.sopra.javagame.control.ai2.DecisionResult.DISCARD;
 import static de.sopra.javagame.model.ArtifactCardType.*;
@@ -16,6 +17,7 @@ import static de.sopra.javagame.model.ArtifactCardType.*;
  */
 @DoAfter(act = DISCARD, value = DiscardUseSandbagToKeepFourTreasureCards.class)
 public class DiscardOddTreasureCardWhenPlayerHasThreeOfSomething extends Decision {
+    private ArtifactCardType discarded; 
     @Override
     public Decision decide() {
         EnhancedPlayerHand activeHand = playerHand();
@@ -24,8 +26,18 @@ public class DiscardOddTreasureCardWhenPlayerHasThreeOfSomething extends Decisio
         int earth = activeHand.getAmount(EARTH);
         int air = activeHand.getAmount(AIR);
         if (any(air == THREE_CARDS, earth == THREE_CARDS, fire == THREE_CARDS, water == THREE_CARDS)) {
-            if (any(air <= TWO_CARDS, earth <= TWO_CARDS, fire <= TWO_CARDS, water <= TWO_CARDS)) {
+            if (air <= TWO_CARDS) {
+                discarded=AIR;
                 return this;
+            }else if(earth <= TWO_CARDS){
+                discarded=EARTH;
+                return this;
+            }else if(fire <= TWO_CARDS){
+                discarded= FIRE;
+                return this;
+            }else if(water<=TWO_CARDS){
+                discarded= WATER;
+                return this;           
             }
         }
         return null;
@@ -33,6 +45,6 @@ public class DiscardOddTreasureCardWhenPlayerHasThreeOfSomething extends Decisio
 
     @Override
     public ActionQueue act() {
-        return startActionQueue(); //TODO
+        return startActionQueue().discard(discarded);
     }
 }
