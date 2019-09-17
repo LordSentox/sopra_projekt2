983ee3b3875f8f73a1fc06e2facd468e7ac1f69f
sopr049 <sopr049@fiws220.cs.tu-dortmund.de>
2019-09-17 10:22:12 +0200

    add acts in Move classes of ai

--- src/de/sopra/javagame/control/ai2/decisions/TurnMoveOntoTreasureCollectionPointIfPlayerHasFour.java
+++ src/de/sopra/javagame/control/ai2/decisions/TurnMoveOntoTreasureCollectionPointIfPlayerHasFour.java
@@ -1,12 +1,20 @@
 package de.sopra.javagame.control.ai2.decisions;
 
 import de.sopra.javagame.control.ai.ActionQueue;
+import de.sopra.javagame.control.ai.EnhancedPlayerHand;
 import de.sopra.javagame.control.ai2.DoAfter;
 import de.sopra.javagame.control.ai2.PreCondition;
+import de.sopra.javagame.model.ArtifactCardType;
 import de.sopra.javagame.model.ArtifactType;
+import de.sopra.javagame.model.MapTile;
+import de.sopra.javagame.model.MapTileState;
+import de.sopra.javagame.util.Pair;
+import de.sopra.javagame.util.Point;
 
 import static de.sopra.javagame.control.ai2.DecisionResult.TURN_ACTION;
 import static de.sopra.javagame.control.ai2.decisions.Condition.PLAYER_HAS_FOUR_IDENTICAL_TREASURE_CARDS;
+
+import java.util.List;
 
 /**
  * @author Niklas Falke
@@ -16,21 +24,69 @@ import static de.sopra.javagame.control.ai2.decisions.Condition.PLAYER_HAS_FOUR_
 @DoAfter(act = TURN_ACTION, value = TurnGiveTreasureCardsToPlayersWhoNeedThem.class)
 @PreCondition(allTrue = PLAYER_HAS_FOUR_IDENTICAL_TREASURE_CARDS)
 public class TurnMoveOntoTreasureCollectionPointIfPlayerHasFour extends Decision {
+    private Point moveTowards;
     @Override
     public Decision decide() {
-        //FIXME Bewegung fehlt
-        ArtifactType type = tile().getProperties().getHidden();
-        int amountOfType = playerHand().getAmount(type);
-
-        if (amountOfType == FOUR_CARDS) {
-            return this;
+        List<Pair<Point,MapTile>> temples = control.getTemples();
+        EnhancedPlayerHand hand = playerHand();
+        int minRange = Integer.MAX_VALUE;
+        int range;
+        if (hand.getAmount(ArtifactCardType.EARTH) >= FOUR_CARDS ) {
+            for(Pair<Point,MapTile> temple: temples){
+                if(temple.getRight().getProperties().getHidden()==ArtifactType.EARTH && 
+                 !(temple.getRight().getState()==MapTileState.GONE)) {
+                    range=control.getMinimumActionsNeededToReachTarget(player().getPosition(), temple.getLeft(), player().getType());
+                    if(range< minRange){
+                        minRange=range;
+                        moveTowards=control.getClosestPointInDirectionOf(player().legalMoves(true), temple.getLeft(), player().getType());
+                    }
+                }
+            }
+        }else
+        if (hand.getAmount(ArtifactCardType.FIRE) >= FOUR_CARDS ) { 
+            for(Pair<Point,MapTile> temple: temples){
+                if(temple.getRight().getProperties().getHidden()==ArtifactType.FIRE && 
+                 !(temple.getRight().getState()==MapTileState.GONE)) {
+                    range=control.getMinimumActionsNeededToReachTarget(player().getPosition(), temple.getLeft(), player().getType());
+                    if(range< minRange){
+                        minRange=range;
+                        moveTowards=control.getClosestPointInDirectionOf(player().legalMoves(true), temple.getLeft(), player().getType());
+                    }
+                }
+            }
+        }else
+        if(hand.getAmount(ArtifactCardType.AIR) >= FOUR_CARDS ) {
+            for(Pair<Point,MapTile> temple: temples){
+                if(temple.getRight().getProperties().getHidden()==ArtifactType.AIR && 
+                 !(temple.getRight().getState()==MapTileState.GONE)) {
+                    range=control.getMinimumActionsNeededToReachTarget(player().getPosition(), temple.getLeft(), player().getType());
+                    if(range< minRange){
+                        minRange=range;
+                        moveTowards=control.getClosestPointInDirectionOf(player().legalMoves(true), temple.getLeft(), player().getType());
+                    }
+                }
+            } 
+        }else
+        if(hand.getAmount(ArtifactCardType.WATER) >= FOUR_CARDS ){
+            for(Pair<Point,MapTile> temple: temples){
+                if(temple.getRight().getProperties().getHidden()==ArtifactType.WATER && 
+                 !(temple.getRight().getState()==MapTileState.GONE)) {
+                    range=control.getMinimumActionsNeededToReachTarget(player().getPosition(), temple.getLeft(), player().getType());
+                    if(range< minRange){
+                        minRange=range;
+                        moveTowards=control.getClosestPointInDirectionOf(player().legalMoves(true), temple.getLeft(), player().getType());
+                    }
+                }
+            }
         }
+
+        
         return null;
     }
 
     @Override
     public ActionQueue act() {
-        return startActionQueue(); //TODO
+        return startActionQueue().move(moveTowards);
     }
 
 }
--- src/de/sopra/javagame/control/ai2/decisions/TurnMoveToLandingSiteForDeparture.java
+++ src/de/sopra/javagame/control/ai2/decisions/TurnMoveToLandingSiteForDeparture.java
@@ -24,7 +24,7 @@ import static de.sopra.javagame.control.ai2.decisions.Condition.GAME_HAS_ALL_ART
 @DoAfter(act = TURN_ACTION, value = TurnFlyActivePlayerToLandingSiteForDraining.class)
 @PreCondition(allTrue = GAME_HAS_ALL_ARTIFACTS)
 public class TurnMoveToLandingSiteForDeparture extends Decision {
-
+    private Point moveTowards;
     @Override
     public Decision decide() {
         Action action = control.getCurrentAction();
@@ -35,6 +35,7 @@ public class TurnMoveToLandingSiteForDeparture extends Decision {
         Player player = action.getActivePlayer();
         Point playerPosition = player.getPosition();
         if (!landingSitePosition.equals(playerPosition)) {
+            moveTowards= control.getClosestPointInDirectionOf(player().legalMoves(true), control.getTile(PlayerType.PILOT).getLeft(), player().getType());
             return this;
         }
 
@@ -43,7 +44,7 @@ public class TurnMoveToLandingSiteForDeparture extends Decision {
 
     @Override
     public ActionQueue act() {
-        return startActionQueue(); //TODO
+        return startActionQueue().move(moveTowards);
     }
 
 }
